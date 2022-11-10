87b65d2db8ce8eeb506447b4d224f9f980891481
bouledepate <promtheus815@gmail.com>
2022-11-10 20:07:06 +0600

    Woopple-1

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 // todo: Рассмотреть возможность выноса объявления констант из данного файла. Предварительно реализовать
 // todo: инициализацию приложения
-defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require(__DIR__ . '/../vendor/autoload.php');
